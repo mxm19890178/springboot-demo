a462dd97642419e5896be0997465b842fb2cecbf
maxiaomin <maxiaomin@zhrt.com.cn>
2021-01-09 22:05:21 +0800

    第一次推

---
+++ src/test/java/com/mxm/lxl/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxm.lxl.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
